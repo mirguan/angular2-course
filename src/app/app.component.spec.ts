873f772c46eaad7b1f594f545a9eb2f4c0b0083d
Alexey Emtsov <Alexey_Emtsov@epam.com>
2017-02-15 15:53:04 +0300

    Fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,15 +2,28 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { StoreModule } from '@ngrx/store';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { appReducer, CourseEffects, LoginEffects, AppEffects } from './state';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './components/app-header.component';
+import { AppBreadcrumbComponent } from './common/breadcrumb.component';
+import * as login from './components/login';
 
 describe('AppComponent', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [
-                AppComponent
+                AppComponent,
+                AppHeaderComponent,
+                AppBreadcrumbComponent,
+                login.LoginPaneComponent
             ],
-            imports: [RouterTestingModule]
+            imports: [
+                RouterTestingModule,
+                NgbModule.forRoot(),
+                StoreModule.provideStore(appReducer)
+            ]
         });
         TestBed.compileComponents();
     });
